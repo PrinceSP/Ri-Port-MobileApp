0fcdec9462353b986493f0ed745f2e0c072992be
PrinceSP <princepasombaran@gmail.com>
2021-10-25 17:53:05 +0800

    import-export icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,10 @@
 import MainLogo from './main-logo.svg'
+import AvatarProfile from './avatarIconsProfile.svg'
+import Help from './help.svg'
+import Report from './reports.svg'
+import Share from './share.svg'
+import SignOut from './signOut.svg'
+import Single from './single_logo.png'
+import PD from './pdLogo.png'
 
-export {MainLogo}
+export {MainLogo,AvatarProfile,Help,Report,Share,SignOut,Single,PD}
