8072af08285d12761fcf1f2da030c95789094348
PrinceSP <princepasombaran@gmail.com>
2021-10-26 20:59:54 +0800

    modified weather molecules to get the API results

--- src/components/molecules/weather/index.js
+++ src/components/molecules/weather/index.js
@@ -4,23 +4,41 @@ import {Text,View} from 'react-native'
 
 const WeatherAPI=()=>{
   const [data,setData] = useState()
+  const [desc,setDesc] = useState('')
 
-  // async function fetchData(){
-  //   let res = await fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=London", {
-  //   	"method": "GET",
-  //   	"headers": {
-  //   		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-  //   		"x-rapidapi-key": "cd55f51d64msh18564c4288222a4p1e8936jsnc9d604ef618f"
-  //   	}
-  //   })
-  //   .then(response => {
-  //   	console.log(response);
-  //     setData(response)
-  //   })
-  //   .catch(err => {
-  //   	console.error(err);
-  //   });
-  // }
+  useEffect(()=>{
+    apiHolder()
+  },[])
+
+  function apiCall(url,res,rej){
+    let datas = new XMLHttpRequest()
+    datas.onreadystatechange = function (){
+      if (datas.readyState === 4) {
+        if (datas.status === 200) {
+          res(datas.response)
+        }else{
+          rej()
+        }
+      }
+    }
+    datas.open('get',url)
+    datas.send()
+  }
+
+  function apiHolder(){
+    const apiKey = `8f5a73fd3b0e7a36a6127887c4ee73be`
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=madrid&appid=${apiKey}`
+
+    apiCall(apiUrl,results=>{
+      const datas = JSON.parse(results)
+      const icon = `http://openweathermap.org/img/wn/${datas.weather[0].icon}.png`
+      setData(datas)
+      setDesc(datas.weather[0].main)
+      console.log(desc);
+    },()=>{
+      console.log('error');
+    })
+  }
 
   return(
     <View>
