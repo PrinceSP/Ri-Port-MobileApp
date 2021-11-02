020d5fd50dffa9e8c06085506a5c5ca0f0c111e9
PrinceSP <princepasombaran@gmail.com>
2021-11-02 16:45:39 +0800

    styling the text and button at report list components

--- src/components/atoms/reportList/index.js
+++ src/components/atoms/reportList/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import {View,Text,TouchableOpacity} from 'react-native'
+import Button from '../button'
 
 const ReportList =({status,title,date})=>{
   return(
     <View style={{flexDirection:'row',justifyContent:'space-between',
       alignItems:'center',width:365,borderBottomWidth:1,borderColor:'#F1DADA',paddingBottom:7.73}}>
-      <Text style={{fontFamily:'Poppins-Regular',fontSize:12}}>{status}</Text>
+      <Text style={{fontFamily:'Poppins-Regular',fontSize:12,color:'#000'}}>{status}</Text>
       <View style={{width:216,minHeight:42}}>
-        <Text style={{fontFamily:'Poppins-Regular',fontSize:16}}>{title}</Text>
-        <Text style={{fontFamily:'Poppins-Regular',fontSize:14}}>{date}</Text>
+        <Text style={{fontFamily:'Poppins-Regular',fontSize:16,color:'#000'}}>{title}</Text>
+        <Text style={{fontFamily:'Poppins-Regular',fontSize:14,color:'#000'}}>{date}</Text>
       </View>
-      <Text>details</Text>
+      <Button name='details' color='#3043F0'/>
     </View>
   )
 }
