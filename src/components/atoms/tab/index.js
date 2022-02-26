d46639426b2d7a788f8e1e9107cb9704420c4030
PrinceSP <princepasombaran@gmail.com>
2022-02-27 02:10:22 +0800

    change the style of the bottom bar tab/button

--- src/components/atoms/tab/index.js
+++ src/components/atoms/tab/index.js
@@ -1,51 +1,55 @@
 import React from 'react'
-import {TouchableOpacity,View,StyleSheet,Animated} from 'react-native'
+import {TouchableHighlight,View,StyleSheet,Text,Animated,Platform} from 'react-native'
 import {HomeIcon,ReportIcon,NotifIcon} from '../../../assets'
 
 const Tab = ({tab,color,onPress})=>{
   const otherStyles= {
     shadowOffset: { width: 10, height: 10 },
-    shadowColor: color,
+    shadowColor: "#B6E203",
     shadowOpacity: 1,
     shadowRadius:20,
     elevation: 10,
     borderRadius:50,
   }
 
-  const containerStyle={
-    height:4,
-    width:24,
-    backgroundColor:color,
-    marginTop:6,
-    borderRadius:20
-  }
-
+  const colors = color==='#000';
   const style = StyleSheet.create({
     report:{
-      bottom:23,
-      shadowOffset: { width: 10, height: 10 },
-      shadowColor: color,
-      shadowOpacity: 1,
-      shadowRadius:20,
-      elevation: 10,
+      position:'absolute',bottom:0,
+      height:70,
+      width:70,
+      borderWidth:5,
+      // borderColor:'#B5CEFF',
+      borderColor:'#eee',
       borderRadius:50,
-      backgroundColor : "#17F348"
+      justifyContent:'center',
+      alignItems:'center',
+      backgroundColor:'#FFC700',
+      ...Platform.select({
+        ios: {
+          shadowColor:"#fa9",
+          shadowOffset: { width: 10, height: 10 },
+          shadowOpacity: 1,
+          shadowRadius:20
+        },
+        android: {
+          elevation:10
+        }
+      })
     },
-    other:color==='#fff'?otherStyles:{},
-    container:color==='#fff'?containerStyle:{}
+    other:colors?otherStyles:{},
   });
 
-  const {report,other,container} = style;
-
+  const {report,other} = style;
 
   return(
-    <TouchableOpacity onPress={onPress} style={{alignItems:'center'}}>
+    <TouchableHighlight underlayColor="#fff" activeOpacity={0.6} onPress={onPress} style={{alignItems:'center'}}>
       {
-        tab.name === 'Home'?<View style={{alignItems:'center'}}><HomeIcon height={28} fill={color} style={other}/><Animated.View style={container}/></View>
-      :tab.name=== 'Report'?<ReportIcon style={report} height={72} width={72}/>
-      :<View style={{alignItems:'center'}}><NotifIcon height={28} fill={color} style={other}/><Animated.View style={container}/></View>
+        tab.name === 'Home'?<View style={{alignItems:'center'}}><HomeIcon height={27} fill={color} style={other}/></View>
+      :tab.name=== 'Report'?<View style={report}><ReportIcon height={60}/></View>
+    :<View style={{alignItems:'center'}}><NotifIcon height={28} width={38} fill={color} style={other}/></View>
       }
-    </TouchableOpacity>
+    </TouchableHighlight>
   )
 }
 
