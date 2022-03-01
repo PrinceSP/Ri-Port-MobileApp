c1e351c546cfc470cecb5957e2ee7d77d76e1b42
PrinceSP <princepasombaran@gmail.com>
2022-03-01 13:17:51 +0800

    make the password can't be seen and save the data to global state with context

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,12 +1,13 @@
 import React, {useState,useContext} from 'react'
-import {Text,View,Image,StyleSheet,SafeAreaView} from 'react-native'
-import {MainLogo} from '../../assets'
+import {Text,View,Image,StyleSheet,SafeAreaView,ActivityIndicator} from 'react-native'
+import {MainLogo,EyeTrue,EyeFalse} from '../../assets'
 import {Input,Gap,Button,Header} from '../../components'
 import {AuthContext} from '../../context/authContext'
 
 const Login =({navigation})=>{
   const [username,setUsername] = useState('')
   const [password,setPassword] = useState('')
+  const [hide,setHide] = useState(true)
   const {isFetching,dispatch} = useContext(AuthContext)
   const url = 'https://riport-app.herokuapp.com/api/auth/login'
 
@@ -23,19 +24,19 @@ const Login =({navigation})=>{
       }
       const response = await fetch(url,options)
       const results = await response.json()
-      if (response.status === 200) {
+      if (response.status == 200) {
+        // console.log(results.datas);
         dispatch({ type: "LOGIN_SUCCESS", payload: results.datas });
-        setPassword('')
-        setUsername('')
         navigation.navigate('Root',{screen:'BottomTabs'})
-      }else {
-        console.log(results);
+      }else{
+        isFetching=false
       }
-    } catch (e) {
-      dispatch({ type: "LOGIN_FAILURE", payload: e });
       setPassword('')
       setUsername('')
+    } catch (e) {
+      dispatch({ type: "LOGIN_FAILURE", payload: e });
     }
+    setHide(true)
   }
   return(
     <View style={{backgroundColor:'#fff',flex:1}}>
@@ -48,7 +49,11 @@ const Login =({navigation})=>{
           <Gap height={65}/>
           <Input placeholder="Username" defaultValue={username} onChangeText={event=>setUsername(event)}/>
           <Gap height={30}/>
-          <Input placeholder="Password" defaultValue={password} onChangeText={event=>setPassword(event)}/>
+          {isFetching&&<ActivityIndicator style={{zIndex:1,position:'absolute',top:200,backgroundColor:'#bcbcbc',padding:10,borderRadius:50}} size="large" color="#fff"/>}
+          <View>
+            <Input placeholder="Password" defaultValue={password} secureTextEntry={hide} onChangeText={event=>setPassword(event)}/>
+            {hide ? <EyeFalse height={20} onPress={()=>setHide(false)} style={{position:'absolute',right:0,top:15}}/> : <EyeTrue height={20} onPress={()=>setHide(true)} style={{position:'absolute',right:0,top:15}}/>}
+          </View>
           <Gap height={26}/>
           <Text style={style.poppinsMed}>forgot password?</Text>
           <Gap height={29}/>
