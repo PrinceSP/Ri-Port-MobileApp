aaa9298f098a8e555dec5fe6a36e99f84ce559d2
PrinceSP <princepasombaran@gmail.com>
2022-03-01 16:06:32 +0800

    add theme provider to wrap the app.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './routes'
 import {AuthContextProvider} from './context/authContext'
+import {ThemeContextProvider} from './context/themeContext'
 
 const App = () => {
 
   return (
     <AuthContextProvider>
-      <NavigationContainer>
-        <Routes/>
-      </NavigationContainer>
+      <ThemeContextProvider>
+        <NavigationContainer>
+          <Routes/>
+        </NavigationContainer>
+      </ThemeContextProvider>
     </AuthContextProvider>
   );
 
