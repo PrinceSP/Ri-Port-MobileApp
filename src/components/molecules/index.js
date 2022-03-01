bd5b1ca61ffb02d6b791e4d630930c150b71af56
PrinceSP <princepasombaran@gmail.com>
2022-03-01 13:13:44 +0800

    remove weather component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,7 +2,6 @@ import OnBoarding from './onBoarding'
 import Header from './header'
 import DrawerContent from './drawerContent'
 import TabsContent from './tabsContent'
-import WeatherAPI from './weather'
 import ImagePicker from './imagePicker'
 import ModalSuccess from './modalFeedback'
 import FeedbackForm from './feedbackForm'
@@ -10,5 +9,5 @@ import ReportForm from './reportForm'
 import MapFinder from './maps'
 import ReportPost from './reportsPosts'
 
-export {OnBoarding,Header,DrawerContent,WeatherAPI,TabsContent,
+export {OnBoarding,Header,DrawerContent,TabsContent,
   MapFinder,ImagePicker,ModalSuccess,FeedbackForm,ReportForm,ReportPost}
