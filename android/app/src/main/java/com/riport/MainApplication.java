2495f74000592bc8e52f101c5dbf5e4c462ab36f
PrinceSP <princepasombaran@gmail.com>
2022-03-02 12:56:05 +0800

    linking react-native-async-storage

--- android/app/src/main/java/com/riport/MainApplication.java
+++ android/app/src/main/java/com/riport/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import cl.json.RNSharePackage;
 import cl.json.ShareApplication;
 import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
