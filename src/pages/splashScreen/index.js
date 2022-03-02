9b19809f62ead0ba2d3a635e5f339758116faeb9
PrinceSP <princepasombaran@gmail.com>
2022-03-02 13:06:12 +0800

    check if user already seen the onboarding page and not call the onboarding again if true from async storage

--- src/pages/splashScreen/index.js
+++ src/pages/splashScreen/index.js
@@ -1,12 +1,30 @@
-import React, {useEffect} from 'react'
+import React, {useEffect,useState} from 'react'
 import {View} from 'react-native'
 import {MainLogo} from '../../assets'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SplashScreen = ({navigation})=>{
+  const[loading,setLoading] = useState(true)
+  const[viewedOnBoarding,setViewedOnBoarding] = useState(false)
+
+  const checkOnBoarding = async()=>{
+    try {
+      const value = await AsyncStorage.getItem("@viewed")
+      if (value!==null) {
+        setViewedOnBoarding(true)
+      }
+    } catch (e) {
+      console.log(e);
+    } finally{
+      setLoading(false)
+    }
+  }
+
   useEffect(()=>{
+    checkOnBoarding()
     setTimeout(()=>{
-      navigation.replace('OnBoardingPage');
-    },2000)
+      loading?navigation.replace('WelcomeScreen'):navigation.replace('OnBoardingPage');
+    },1000)
   },[])
   return(
     <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
