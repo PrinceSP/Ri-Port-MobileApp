c2149ec9d923e845035400cd24f7c0232ee053fb
PrinceSP <princepasombaran@gmail.com>
2022-03-02 22:50:11 +0800

    remove all the async function

--- src/pages/splashScreen/index.js
+++ src/pages/splashScreen/index.js
@@ -1,31 +1,15 @@
 import React, {useEffect,useState} from 'react'
 import {View} from 'react-native'
 import {MainLogo} from '../../assets'
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SplashScreen = ({navigation})=>{
-  const[loading,setLoading] = useState(true)
-  const[viewedOnBoarding,setViewedOnBoarding] = useState(false)
-
-  const checkOnBoarding = async()=>{
-    try {
-      const value = await AsyncStorage.getItem("@viewed")
-      if (value!==null) {
-        setViewedOnBoarding(true)
-      }
-    } catch (e) {
-      console.log(e);
-    } finally{
-      setLoading(false)
-    }
-  }
 
   useEffect(()=>{
-    checkOnBoarding()
     setTimeout(()=>{
-      loading?navigation.replace('WelcomeScreen'):navigation.replace('OnBoardingPage');
-    },1000)
+      navigation.replace('OnBoardingPage')
+    },700)
   },[])
+
   return(
     <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
       <MainLogo/>
