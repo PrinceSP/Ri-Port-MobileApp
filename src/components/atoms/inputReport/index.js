f6d0ebf38aaf405786d67d53860f186af3be13df
PrinceSP <princepasombaran@gmail.com>
2022-03-04 15:12:40 +0800

    change input report color

--- src/components/atoms/inputReport/index.js
+++ src/components/atoms/inputReport/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {TextInput,Text,View} from 'react-native'
 
-const ReportInput = ({label,...rest}) => {
+const ReportInput = ({label,color="#000",...rest}) => {
   return (
     <View style={{height:66,borderBottomWidth:1,borderColor:'#F1DADA'}}>
       <Text style={{fontSize:17,fontFamily:'Poppins-Medium',color:'#8ACEEC'}}>{label}</Text>
-      <TextInput style={{fontSize:20,color:'#000'}}{...rest}/>
+      <TextInput style={{fontSize:20,color}}{...rest}/>
     </View>
   )
 }
