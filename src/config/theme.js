ee936d13b6587417104475945f1eddb468016c0c
PrinceSP <princepasombaran@gmail.com>
2022-03-06 22:59:55 +0800

    add theme configuration for default and dark theme color and mode

---
+++ src/config/theme.js
@@ -0,0 +1,11 @@
+export const defaultTheme ={
+  backgroundColor:"#fff",
+  color:"#000",
+  themeMode:"default"
+}
+
+export const darkTheme ={
+  backgroundColor:"#000",
+  color:"#fff",
+  themeMode:"dark"
+}
