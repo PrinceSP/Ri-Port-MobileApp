4342e090ec6cba7967695bc4f578fb87051eb6c8
PrinceSP <princepasombaran@gmail.com>
2022-03-06 14:12:50 +0800

    setting the theme colors

--- src/context/themeContext.js
+++ src/context/themeContext.js
@@ -1,21 +1,25 @@
 import React, {createContext,useState} from 'react'
-
 export const ThemeContext = createContext()
 
 export const ThemeContextProvider = ({children}) => {
+
   const [theme,setTheme] = useState(false)
   const [color,setColor] = useState('#000')
   const [bgColor,setBgColor] = useState('#fff')
 
   const toggleScheme = ()=>{
-    if(theme===true){
+    if(theme){
       setTheme(false)
       setBgColor('#fff')
       setColor('#000')
-    } else{
+      // console.log(toggle._j);
+    } else if(toggle=="false"){
       setTheme(true)
       setBgColor('#121212')
       setColor('#fff')
+      // console.log(toggle._j);
+    } else{
+      return theme
     }
   }
   return (
