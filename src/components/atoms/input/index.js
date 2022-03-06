281910caa9cdf9e31234345f247c2df6d97df3d4
PrinceSP <princepasombaran@gmail.com>
2022-03-06 14:07:59 +0800

    change color to dynamic

--- src/components/atoms/input/index.js
+++ src/components/atoms/input/index.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import {TextInput,StyleSheet} from 'react-native'
+import {View,Text,TextInput,StyleSheet} from 'react-native'
 
-const Input = ({...rest})=>{
+const Input = ({setLabel=false,borderRadius=50,label,...rest})=>{
   const style=StyleSheet.create({
     container:{
       height:50,
       width:329,
       borderWidth:1.5,borderColor:'#B5DCFF',
-      borderRadius:50,color:'#000',
+      borderRadius,color:'#000',
       paddingHorizontal:28.77,fontSize:19,backgroundColor:'#fff',
     }
   })
 
   return(
-    <TextInput placeholderTextColor="#999" style={style.container} {...rest}/>
+    <View>
+      {setLabel && <Text style={{fontSize:20,color:"#000",marginBottom:4}}>{label}</Text>}
+      <TextInput placeholderTextColor="#999" style={style.container} {...rest}/>
+    </View>
   )
 }
 
