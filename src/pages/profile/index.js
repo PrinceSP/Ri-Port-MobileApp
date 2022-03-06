97c669d1cdd8004099dfbcbdd1690ed21559c10d
PrinceSP <princepasombaran@gmail.com>
2022-03-06 14:10:48 +0800

    redesign the profile screen and remove edit button

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,7 +1,7 @@
 import React, {useState,useContext} from 'react'
 import {View,Text,Image} from 'react-native'
 import {Button,BioHolder,Gap,Header} from '../../components'
-import {DateIcon,Email,Address,Phone,AvatarProfile,ID,MainLogo} from '../../assets'
+import {DateIcon,Address,Phone,AvatarProfile,ID,MainLogo} from '../../assets'
 import {AuthContext} from '../../context/authContext'
 import {ThemeContext} from '../../context/themeContext'
 
@@ -12,20 +12,41 @@ const Profile = ({navigation})=>{
   return(
     <View style={{flex:1,backgroundColor:bgColor}}>
       <Gap height={15}/>
-      <Header name='Profile' action='< back' edit={true} nav={navigation} color={color} bgColor={bgColor}/>
-      <Gap height={350}/>
-      <View style={{flex:0.9,alignItems:'center',justifyContent:'flex-end'}}>
+      <Header name='Profile' action='< back' nav={navigation} color={color} bgColor={bgColor}/>
+      <View style={{flex:0.86,alignItems:'center',justifyContent:'flex-end'}}>
         <View style={{borderStyle:'dashed',borderWidth:2,borderColor:'#8CC4F8',borderRadius:75,height:130,width:130,alignItems:'center',justifyContent:'center'}}>
           {user.profilePicture ? <Image source={{uri:`data:image/png;base64,${user.profilePicture}`}} style={{height:115,width:115,backgroundColor:'#eee',borderRadius:65}}/> : <View style={{height:115,width:115,backgroundColor:'#eee',borderRadius:65}}/>}
         </View>
         <Gap height={20}/>
-        <BioHolder icon={<AvatarProfile fill="#a0a0a0"/>} userInfo={user.username} labelInfo='Fullname' color={color} backgroundColor={bgColor}/>
-        <BioHolder icon={<Email height={28}/>} userInfo={user.email} labelInfo='Email address' color={color} backgroundColor={bgColor}/>
-        <BioHolder icon={<Address height={28}/>} userInfo={user.address} labelInfo='Address' color={color} backgroundColor={bgColor}/>
-        <BioHolder icon={<DateIcon height={28}/>} userInfo={user.dateOfBirth} labelInfo='Date of Birth' color={color} backgroundColor={bgColor}/>
-        <BioHolder icon={<Phone height={26}/>} userInfo={user.phoneNumber} labelInfo='Phone Number' color={color} backgroundColor={bgColor}/>
-        <BioHolder icon={<ID height={24}/>} userInfo={user.ktpId} labelInfo='ID Card Number' color={color} backgroundColor={bgColor}/>
-        <Gap height={28}/>
+        <Text style={{color,backgroundColor:bgColor,fontSize:28,fontFamily:'Poppins-Bold'}}>{user.username}</Text>
+        <Text style={{color:"#999",backgroundColor:bgColor,fontSize:16,fontFamily:'Poppins-Regular'}}>{user.email}</Text>
+        <Gap height={20}/>
+        <BioHolder icon={<Address height={22}/>}
+          labelInfo=""
+          userInfo={user.address}
+          labelInfo='Address'
+          color={color}
+          backgroundColor="#66bb8a"
+          onPress={()=>alert('address')}/>
+        <BioHolder icon={<DateIcon height={22}/>}
+          labelInfo=""
+          userInfo={user.dateOfBirth}
+          labelInfo='Date of Birth'
+          color={color}
+          backgroundColor="#f44"
+          onPress={()=>alert('date of birth')}/>
+        <BioHolder icon={<Phone height={20}/>}
+          labelInfo="Phone Number"
+          userInfo={user.phoneNumber}
+          color={color}
+          backgroundColor="#ffa500"
+          onPress={()=>alert('phone')}/>
+        <BioHolder icon={<ID height={18}/>}
+          userInfo={user.ktpId}
+          labelInfo='ID Card Number'
+          color={color}
+          backgroundColor="#009"
+          onPress={()=>alert('id card number')}/>
       </View>
     </View>
   )
