25514441bb0ea40ac772ee8b60a80cc94c7fa18c
PrinceSP <princepasombaran@gmail.com>
2022-03-13 23:30:52 +0800

    add bottom sheet when button is clicked

--- src/pages/reportPage/index.js
+++ src/pages/reportPage/index.js
@@ -1,9 +1,13 @@
 import React, {useState,useEffect,useContext} from 'react'
-import {Text,View,StyleSheet} from 'react-native'
+import {Text,View,StyleSheet,ScrollView,Dimensions,useWindowDimensions} from 'react-native'
 import {Header,Gap,ReportForm,MapFinder,Button,ReportInput} from '../../components'
 import {launchImageLibrary} from 'react-native-image-picker'
 import {AuthContext} from '../../context/authContext'
 import {useTheme} from '../../context/themeContext'
+import {PanGestureHandler} from 'react-native-gesture-handler'
+import Animated,{useAnimatedGestureHandler,useAnimatedStyle,useSharedValue,withSpring} from 'react-native-reanimated'
+
+const {width,height} = Dimensions.get('window')
 
 const ReportPage = ({navigation})=>{
   const {user:currentUser} = useContext(AuthContext)
@@ -66,44 +70,76 @@ const ReportPage = ({navigation})=>{
     return allDatas
   }
 
+  const dimensions = useWindowDimensions()
+  const springConfig ={
+    damping:80,
+    overshootClamping:true,
+    restDisplacementThreshold:0.1,
+    stiffness:500
+  }
+
+  const top = useSharedValue(dimensions.height)
+
+  const bottomSheetStyle = useAnimatedStyle(()=>{
+    return{
+      top:withSpring(top.value,springConfig)
+    }
+  })
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart(_, context){
+      context.startTop = top.value
+    },
+    onActive(event,context){
+      top.value = context.startTop + event.translationY
+    },
+    onEnd(){
+      if (top.value > dimensions.height / 2 + 200) {
+        top.value = dimensions.height
+      } else{
+        top.value = dimensions.height
+      }
+    }
+  })
+
+
   return(
-    // <View style={[container,{backgroundColor:theme.backgroundColor}]}>
-    //   <Gap height={15}/>
-    //   <Header name="report" button={true} navigation={navigation} color={theme.color} bgColor={theme.backgroundColor}/>
-    //   <Gap height={20}/>
-    //   <ScrollView keyboardShouldPersistTaps='always' contentContainerStyle={style.formContainer}>
-    //     <Text style={text1}>Make Your Report</Text>
-    //     <Gap height={33}/>
-    //     <MapFinder getGeometrics={getGeometrics}/>
-    //     <Gap height={28}/>
-    //     <View>
-    //       <ReportInput color={theme.color} label="Fullname *" defaultValue={fname} onChangeText={e=>{
-    //           setData({...data,fname:e})
-    //         }}/>
-    //       <Gap height={28}/>
-    //       <ReportInput color={theme.color} label="Phone Number *" defaultValue={phone} onChangeText={e=>{
-    //           setData({...data,phone:e})
-    //         }}/>
-    //       <Gap height={28}/>
-    //       <ReportInput color={theme.color} label="ID Card *" defaultValue={idCard} onChangeText={e=>{
-    //           setData({...data,idCard:e})
-    //         }}/>
-    //       <Gap height={28}/>
-    //     </View>
-    //     <Gap height={27}/>
-    //     <View style={{width:270}}>
-    //       <Text style={{color:'#8ACEEC',fontFamily:'Poppins-Medium',fontSize:17}}>Upload Road Picture*</Text>
-    //       <View style={{alignItems:'center',flexDirection:'row'}}>
-    //         <Button name="Upload" color={theme.backgroundColor} fam='Poppins-Medium' style={[button,{backgroundColor:theme.color}]} onPress={()=>getImage()}/>
-    //         <Text style={{marginLeft:18,color:theme.color}}>{photoName}</Text>
-    //       </View>
-    //     </View>
-    //     <View style={{alignItems:'center'}}>
-    //       <Button name="Submit Report" color='#fff' fam='Poppins-Bold' size={24} style={buttonSubmit} onPress={()=>submit()}/>
-    //     </View>
-    //   </ScrollView>
-    // </View>
-    <MapFinder getGeometrics={getGeometrics} navigation={navigation}/>
+    <>
+      <Button name="Make Report" onPress={()=>{
+        top.value = withSpring(dimensions.height/25,springConfig)
+      }}/>
+      <MapFinder getGeometrics={getGeometrics} navigation={navigation}/>
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+        <Animated.View style={[bottomSheet,bottomSheetStyle,{backgroundColor:theme.backgroundColor,shadowColor:theme.color}]}>
+          <View style={sheetLine}/>
+          <View>
+            <ReportInput color={theme.bgColor} label="Fullname *" defaultValue={fname} onChangeText={e=>{
+                setData({...data,fname:e})
+              }}/>
+            <Gap height={28}/>
+            <ReportInput color={theme.bgColor} label="Phone Number *" defaultValue={phone} onChangeText={e=>{
+                setData({...data,phone:e})
+              }}/>
+            <Gap height={28}/>
+            <ReportInput color={theme.bgColor} label="ID Card *" defaultValue={idCard} onChangeText={e=>{
+                setData({...data,idCard:e})
+              }}/>
+            <Gap height={28}/>
+          </View>
+          <Gap height={27}/>
+          <View style={{width:270}}>
+            <Text style={upload}>Upload Road Picture*</Text>
+            <View style={{alignItems:'center',flexDirection:'row'}}>
+              <Button name="Upload" color={theme.backgroundColor} fam='Poppins-Medium' style={[button,{backgroundColor:theme.color}]} onPress={()=>getImage()}/>
+              <Text style={{marginLeft:18,color:theme.color}}>{photoName}</Text>
+            </View>
+          </View>
+          <View style={{alignItems:'center'}}>
+            <Button name="REPORT" color='#fff' fam='Poppins-Bold' size={24} style={buttonSubmit} onPress={()=>submit()}/>
+          </View>
+        </Animated.View>
+      </PanGestureHandler>
+    </>
   )
 }
 
@@ -122,15 +158,38 @@ const style=StyleSheet.create({
   buttonSubmit:{
     marginTop:68,
     backgroundColor:'#598EF5',
-    height:67,
-    width:340,
+    height:57,
+    width:220,
     borderRadius:50,
     alignItems:'center',
     justifyContent:'center'
   },
+  bottomSheet:{
+    position:'absolute',
+    bottom:0,
+    right:0,
+    left:0,
+    borderTopLeftRadius:20,
+    borderTopRightRadius:20,
+    zIndex:99,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.9,
+    shadowRadius:3.84,
+    elevation:10,
+    paddingHorizontal:10
+  },
+  sheetLine:{
+    width:70,
+    borderTopWidth:4,
+    borderTopColor:"#777",
+    borderRadius:2,
+    alignSelf:'center',
+    marginVertical:15
+  },
+  upload:{color:'#8ACEEC',fontFamily:'Poppins-Medium',fontSize:17}
 })
 
-const {container,text1,mapContainer,formContainer,button,buttonSubmit} = style
+const {container,text1,mapContainer,formContainer,button,buttonSubmit,bottomSheet,sheetLine,upload} = style
 
 
 export default ReportPage
