4450c38cb7e745fc106224a695058560a7e05cf8
PrinceSP <princepasombaran@gmail.com>
2022-03-15 19:58:00 +0800

    make dummy component for report details

---
+++ src/components/molecules/reportDetails/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View,StyleSheet,Text,Image} from 'react-native'
+
+const ReportDetails = (props) => {
+  return (
+    <View>
+      <Text>29 Nov 2020</Text>
+      <View style={{backgroundColor:'#aaa'}}/>
+    </View>
+  )
+}
+
+export default ReportDetails
