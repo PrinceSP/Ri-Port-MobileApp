5eccb64dab47d9cc79b311fe95beca2b8079d606
PrinceSP <princepasombaran@gmail.com>
2022-03-27 11:04:49 +0800

    change height

--- src/components/atoms/inputReport/index.js
+++ src/components/atoms/inputReport/index.js
@@ -3,7 +3,7 @@ import {TextInput,Text,View} from 'react-native'
 
 const ReportInput = ({label,color="#000",...rest}) => {
   return (
-    <View style={{height:66,borderBottomWidth:1,borderColor:'#F1DADA'}}>
+    <View style={{height:69,borderBottomWidth:1,borderColor:'#F1DADA'}}>
       <Text style={{fontSize:17,fontFamily:'Poppins-Medium',color:'#8ACEEC'}}>{label}</Text>
       <TextInput style={{fontSize:20,color}}{...rest}/>
     </View>
