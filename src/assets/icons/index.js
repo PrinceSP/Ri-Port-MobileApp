6f83b7901126d06dc34bc08abc88d41a0cc82775
PrinceSP <princepasombaran@gmail.com>
2022-04-01 17:31:41 +0800

    add mail icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -23,6 +23,7 @@ import ArrowR from './arrow_right'
 import CustomMarker from './marker.png'
 import ArrowUp from './arrowUp.svg'
 import Verify from './verifyIllustration.svg'
+import Mail from './mail.svg'
 
-export {MainLogo,Verify,CustomMarker,ArrowR,ArrowUp,EyeTrue,EyeFalse,AvatarProfile,Help,Report,ShareIcon,SignOut,Single,Edit,ModalSuccessIcon,
+export {MainLogo,Mail,Verify,CustomMarker,ArrowR,ArrowUp,EyeTrue,EyeFalse,AvatarProfile,Help,Report,ShareIcon,SignOut,Single,Edit,ModalSuccessIcon,
   Burger,HomeIcon,ReportIcon,NotifIcon,DateIcon,Address,Phone,ID,SuccessIcon,FailedIcon}
