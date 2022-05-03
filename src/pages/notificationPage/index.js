32325e61d350233f995ec6d34893902a6de1731e
PrinceSP <princepasombaran@gmail.com>
2022-05-03 18:08:49 +0800

    add empty state

--- src/pages/notificationPage/index.js
+++ src/pages/notificationPage/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import {View,Text} from 'react-native'
-import {Header,Gap} from '../../components'
+import {Header,Gap,Empty} from '../../components'
+import {useTheme} from '../../context/themeContext'
 
 const NotificationsPage = ({navigation})=>{
+  const {theme} = useTheme()
   return(
-    <View style={{backgroundColor:'#fff',flex:1}}>
+    <View style={{backgroundColor:theme.backgroundColor,flex:1}}>
       <Gap height={15} button={true} action='Cancel'/>
-      <Header name="Notifications" button={true} navigation={navigation}/>
-      <Gap height={45}/>
-      <Text>Notif Page</Text>
+      <Header name="     Notifications" button={true} navigation={navigation} color={theme.color} bgColor={theme.backgroundColor}/>
+      <Gap height={200}/>
+      <Empty/>
     </View>
   )
 }
