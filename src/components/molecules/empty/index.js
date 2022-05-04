858c23c7a13bc105ded6838965c38fc03e61e379
PrinceSP <princepasombaran@gmail.com>
2022-05-04 17:19:27 +0800

    change the text language

--- src/components/molecules/empty/index.js
+++ src/components/molecules/empty/index.js
@@ -6,8 +6,8 @@ const Empty = () => {
   return (
     <View style={{flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
       <NoData height={120}/>
-      <Text style={{fontSize:20,color:"#777",marginBottom:7,marginTop:15}}>Belum ada data</Text>
-      <Text style={{fontSize:15,color:"#aaa"}}>silahkan masukkan data nota anda</Text>
+      <Text style={{fontSize:20,color:"#777",marginBottom:7,marginTop:15}}>No Data</Text>
+      <Text style={{fontSize:15,color:"#aaa"}}>There is no notification to be shown!</Text>
     </View>
   )
 }
