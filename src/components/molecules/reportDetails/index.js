f057b7165dd2a02f65bb6fdb8c167013089de9c8
PrinceSP <princepasombaran@gmail.com>
2022-05-05 16:06:50 +0800

    add the content data from api

--- src/components/molecules/reportDetails/index.js
+++ src/components/molecules/reportDetails/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import {View,StyleSheet,Text,Image} from 'react-native'
+import {Button} from '../../atoms'
 
-const ReportDetails = (props) => {
+const ReportDetails = ({date,roadPicture,title,desc}) => {
   return (
-    <View>
-      <Text>29 Nov 2020</Text>
-      <View style={{backgroundColor:'#aaa'}}/>
+    <View style={style.container}>
+      <Text>{date}</Text>
+      <Text>{title}</Text>
+      <Image source={{uri:`data:image/png;base64,${roadPicture}`}} style={{backgroundColor:'#aaa'}}/>
+      <Text>{desc}</Text>
+      <Button name="exit" onPress={()=>alert('pressed')}/>
     </View>
   )
 }
+
+const style = StyleSheet.create({
+  container:{
+    flex:1,
+    // backgroundColor:'rgba(114, 213, 255, 1)',
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center'
+  }
+})
 
 export default ReportDetails
