12b1b2584a78c7d8bb5bd63cbf668e69d21f0aad
PrinceSP <princepasombaran@gmail.com>
2022-05-24 18:18:56 +0800

    add activity indicator and fix svg error

--- src/pages/editPassword/index.js
+++ src/pages/editPassword/index.js
@@ -1,5 +1,5 @@
 import React, {useState,useContext} from 'react'
-import {Text,View,StyleSheet,Dimensions} from 'react-native'
+import {Text,View,StyleSheet,Dimensions,ActivityIndicator} from 'react-native'
 import {Input,Gap,Header,Button,toastConfig} from '../../components'
 import {useTheme} from '../../context/themeContext'
 import {EyeTrue,EyeFalse} from '../../assets'
@@ -17,6 +17,7 @@ const EditPassword = ({navigation}) => {
   const [message,setMessage] = useState('')
   const [confirmPassword,setConfirmPassword] = useState('')
   const [hide,setHide] = useState(false)
+  const [isFetching,setIsFetching] = useState(false)
 
   const validation = ()=>{
     if(password.length < 1 && confirmPassword.length < 1)
@@ -37,9 +38,11 @@ const EditPassword = ({navigation}) => {
           },
           body:JSON.stringify(data)
         }
+        setIsFetching(true)
         const req = await fetch(`https://riport-app.herokuapp.com/api/users/${currentUser[0]._id}`,options)
         const results = await req.json()
         if (req.status === 200) {
+          setIsFetching(false)
           Toast.show({
             type:'success',
             text1:'Success',
@@ -48,6 +51,7 @@ const EditPassword = ({navigation}) => {
           setPassword('')
           setConfirmPassword('')
         } else {
+          setIsFetching(false)
           Toast.show({
             type:'error',
             text1:'Error',
@@ -73,6 +77,7 @@ const EditPassword = ({navigation}) => {
       <Toast config={toastConfig} position="top" topOffset={0} visibilityTime={2000}/>
       <Gap height={40}/>
       <View style={styles.inputContainer}>
+        {isFetching===true&&<ActivityIndicator style={{zIndex:1,position:'absolute',top:200,left:'40%',backgroundColor:'#bcbcbc',padding:10,borderRadius:50}} size="large" color="#fff"/>}
         <Text style={[styles.headingTitle,{color:theme.color}]}>Create new password</Text>
         <Gap height={10}/>
         <Text style={[styles.secondaryHeading,{color:theme.backgroundColor==="#fff"?"#777":"#ccc"}]}>Your new password must be different</Text>
@@ -81,12 +86,12 @@ const EditPassword = ({navigation}) => {
         {message ? <Text style={{color:theme.backgroundColor==="#fff"?"#777":"#fff"}}>{message}</Text> : null}
         <View>
           <Input secureTextEntry={hide} borderRadius={10} width={width/1.115} setLabel={true} label="New password" color={theme.color} placeholder="*********" defaultValue={password} onChangeText={(e)=>setPassword(e)}/>
-          {hide?<EyeFalse height={20} onPress={()=>setHide(false)} style={styles.passwordHideToggle}/> : <EyeTrue height={20} onPress={()=>setHide(true)} style={styles.passwordHideToggle}/>}
+          {hide ? <EyeFalse height={20} onPress={()=>setHide(false)} style={{position:'absolute',right:0,top:45}}/> : <EyeTrue height={20} onPress={()=>setHide(true)} style={{position:'absolute',right:0,top:45}}/>}
         </View>
         <Gap height={20}/>
         <View>
           <Input secureTextEntry={hide} borderRadius={10} width={width/1.115} setLabel={true} color={theme.color} label="Confirm new Password" placeholder="*********" defaultValue={confirmPassword} onChangeText={(e)=>setConfirmPassword(e)}/>
-          {hide?<EyeFalse height={20} onPress={()=>setHide(false)} style={styles.passwordHideToggle}/> : <EyeTrue height={20} onPress={()=>setHide(true)} style={styles.passwordHideToggle}/>}
+          {hide ? <EyeFalse height={20} onPress={()=>setHide(false)} style={{position:'absolute',right:0,top:45}}/> : <EyeTrue height={20} onPress={()=>setHide(true)} style={{position:'absolute',right:0,top:45}}/>}
         </View>
       </View>
       <Gap height={75}/>
