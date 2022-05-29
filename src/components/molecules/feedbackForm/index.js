2ee3a7a312e11da1cecbff7eece09c517752913a
PrinceSP <princepasombaran@gmail.com>
2022-05-29 13:58:59 +0800

    change color when theme is changed

--- src/components/molecules/feedbackForm/index.js
+++ src/components/molecules/feedbackForm/index.js
@@ -2,17 +2,20 @@ import React from 'react'
 import {View,Text,StyleSheet,TextInput} from 'react-native'
 import {FeedbackIllustration} from '../../../assets'
 import {Gap} from '../../atoms'
+import {useTheme} from '../../../context/themeContext'
 
 const FeedbackForm =({desc,onChangeText})=>{
+  const {theme} = useTheme()
+
   return(
-    <View style={feedbackContainer}>
+    <View style={[feedbackContainer,{backgroundColor:theme.backgroundColor==="#fff"?"#fff":"#453747"}]}>
       <View style={headingWrapper}>
-        <Text style={[{color:'#9E20A9'},headingText]}>Send me your</Text>
-        <Text style={[{color:'#0085FF',marginLeft:4},headingText]}>Feedback!</Text>
+        <Text style={[{color:theme.backgroundColor==="#fff"?"#9E20A9":"#c97af0"},headingText]}>Send me your</Text>
+        <Text style={[{color:theme.backgroundColor==="#fff"?"#0085FF":"#5aadfa",marginLeft:4},headingText]}>Feedback!</Text>
       </View>
       <Text style={comment}>Tell me how was your experience and leave me a comment</Text>
       <Gap height={37}/>
-      <View style={feedbackTextContainer}>
+      <View style={[feedbackTextContainer,{backgroundColor:theme.backgroundColor==="#fff"?"#FBEFEF":"#564848"}]}>
         <TextInput style={textArea}
           underlineColorAndroid="transparent"
           placeholderTextColor="#C0A8C2"
@@ -34,14 +37,13 @@ const style = StyleSheet.create({
     alignItems:'center',
     paddingTop:28,
     borderRadius:20,
-    backgroundColor : "#fff",
     height:328,
     width:339
   },
   headingWrapper:{flexDirection:'row'},
   headingText:{fontSize:16,fontFamily:'Poppins-SemiBold'},
   comment:{textAlign:'center',width:230,marginTop:5,color:'#999'},
-  feedbackTextContainer:{height:170,marginBottom:20,width:300,borderRadius:6,backgroundColor:'#FBEFEF'},
+  feedbackTextContainer:{height:170,marginBottom:20,width:300,borderRadius:6},
   textArea: {
     textAlignVertical: 'top',
     fontSize:18,
